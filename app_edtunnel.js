5722077ceab394c4628572d88deead66711249b1
ll33896 <78669613+maulon520@users.noreply.github.com>
2023-07-27 22:13:14 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'ce4771d8-aa57-4dbc-92ee-1be520b0e0e2';
+let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
